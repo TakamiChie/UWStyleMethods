c92f55783f323a038cbe519b0b7e7059006033d4
TakamiChie@Visnu <chie@onpu-tamago.net>
2019-01-19 09:56:18 +0900

    Add excel.add() method

--- excel/__init__.py
+++ excel/__init__.py
@@ -56,6 +56,10 @@ class Excel:
     "Get/Set alert control flag"
     self.raw.displayalert = value
 
+  def add(self):
+    "Add a book."
+    return Workbook(self.excel, self.excel.workbooks.Add())
+
   ### Utility Methods.
 
   def chooseworkbook(self, message="Choose Workbook"):
