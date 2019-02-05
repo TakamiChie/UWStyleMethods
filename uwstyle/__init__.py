a8b315e4fa1b21395867a210673bd2a3d29abb20
TakamiChie@Flarous <chie@onpu-tamago.net>
2019-02-05 10:55:59 +0900

    Move method uwstyle.dialog.* to uwstyle.*

--- uwstyle/__init__.py
+++ uwstyle/__init__.py
@@ -1,1 +1,170 @@
+if __name__ == "__main__":
+  import sys
+  import pathlib
+  sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
+
+from uwstyle.dialogs.dialogs import Dialogs
+from uwstyle.dialogs.slctbox import Combobox, Choose
+
 VERSION = "0.0.4"
+
+DIALOGBUTTON_OK = Dialogs.BUTTON_OK
+DIALOGBUTTON_OKCANCEL = Dialogs.BUTTON_OK | Dialogs.BUTTON_CANCEL
+DIALOGBUTTON_YESNO = Dialogs.BUTTON_YES | Dialogs.BUTTON_NO
+DIALOGBUTTON_RETRYIGNOREABORT = Dialogs.BUTTON_ABORT | Dialogs.BUTTON_IGNORE | Dialogs.BUTTON_RETRY
+
+def dialog(message, timeout=0, buttons=DIALOGBUTTON_OK):
+  """
+  Display a message box with an OK button.
+  This method is equivalent to `MSGBOX(message, BTN_OK)` in UWSC.
+
+  Parameters
+  ----
+  message: str
+    Dialog messages
+  timeout: int
+    The dialog timeout.
+  buttons: int
+    Button flags.
+    you can use `uwstyle.dialogs.DIALOGBUTTON_*` constants
+
+  Returns
+  ----
+  state: bool|str|None
+    The button state
+    * True if OK, YES.
+    * False if CANCEL, NO.
+    * "retry" if RETRY.
+    * "ignore" if IGNORE.
+    * "abort" if ABORT.
+    None if the dialog times out when the X button is pressed.
+  """
+  box = Dialogs(__name__, message, buttons)
+  box.timeout = timeout
+  return _retcode2bool(box.show())
+
+def select(message, items=[], timeout=0, buttons=DIALOGBUTTON_OK):
+  """
+  Show Select box.
+  This method is equivalent to `SLCTBOX(SLCT_CMB + SLCT_STR, timeout, message, items)` in UWSC
+
+  Parameters
+  ----
+  message: str
+    Dialog messages
+  items: list(str)
+    Items
+  timeout: int
+    The dialog timeout.
+  buttons: int
+    Button flags.
+    you can use `uwstyle.dialogs.DIALOGBUTTON_*` constants
+
+  Returns
+  ----
+  result: Tuple|None
+    If you cancel the dialog, return None
+    1: bool|str
+      The button state
+      * True if OK, YES.
+      * False if CANCEL, NO.
+      * "retry" if RETRY.
+      * "ignore" if IGNORE.
+      * "abort" if ABORT.
+    2: str
+      Selected Items
+    3: int
+      Selected Item index
+  """
+  box = Combobox(__name__, message, buttons)
+  box.timeout = timeout
+  box.items = items
+  return _retcode2bool(box.show())
+
+
+def choose(message, items=[], timeout=0):
+  """
+  Show Choose box.
+  This method is equivalent to `SLCTBOX(SLCT_BTN + SLCT_STR, timeout, message, items)` in UWSC
+
+  Parameters
+  ----
+  message: str
+    Dialog messages
+  items: list(str)
+    Items
+  timeout: int
+    The dialog timeout.
+
+  Returns
+  ----
+  result: Tuple|None
+    If you cancel the dialog, return None
+    1: int
+      The button index
+    2: str
+      Selected Items
+  """
+  box = Choose(__name__, message)
+  box.timeout = timeout
+  box.items = items
+  return box.show()
+
+def _retcode2bool(retcode):
+  """
+  Converts the return code of a dialog to the return value of a method.
+  * True if OK, YES.
+  * False if CANCEL, NO.
+  * "retry" if RETRY.
+  * "ignore" if IGNORE.
+  * "abort" if ABORT.
+  * None if None.
+  is returned.
+
+  Parameters
+  ----
+  retcode: int|tuple
+    retcode
+  """
+  ret = None
+  if type(retcode) is tuple:
+    ret = retcode[0]
+  else:
+    ret = retcode
+
+  if ret == Dialogs.BUTTON_YES or ret == Dialogs.BUTTON_OK:
+    ret = True
+  elif ret == Dialogs.BUTTON_NO or ret == Dialogs.BUTTON_CANCEL:
+    ret = False
+  elif ret == Dialogs.BUTTON_ABORT:
+    ret = "abort"
+  elif ret == Dialogs.BUTTON_RETRY:
+    ret = "retry"
+  elif ret == Dialogs.BUTTON_IGNORE:
+    ret = "ignore"
+
+  if type(retcode) == tuple:
+    ret = tuple([ret]) + retcode[1:]
+
+  return ret
+
+if __name__ == "__main__":
+  print(dialog("test"))
+  print(dialog("test", buttons=DIALOGBUTTON_YESNO, timeout=5))
+  print(dialog("test", buttons=DIALOGBUTTON_RETRYIGNOREABORT))
+  print(select("test", items=["A", "B", "C"]))
+  print(select("test", items=["A", "B", "C"], timeout=5, buttons=DIALOGBUTTON_YESNO))
+  print(choose("test", items=["A", "B", "C", "D", "E"]))
+  print(choose("test", items=["A", "B", "C", "D", "E"], timeout=5))
+  print(dialog("""
+  this is a long long text dialog
+  """ * 20))
+  print(dialog("""
+  this is a long long text dialog
+  """ * 30))
+  print(dialog("""
+  this is a long long text dialog
+  """ * 40))
+  print(dialog("""
+  this is a long long text dialog
+  """ * 100))
--- uwstyle/dialogs/__init__.py
+++ uwstyle/dialogs/__init__.py
@@ -1,15 +1,15 @@
 if __name__ == "__main__":
   import sys
   import pathlib
-  sys.path.append(str(pathlib.Path(__file__).parent.parent.parent))
+  sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.parent))
 
-from uwstyle.dialogs.dialogs import Dialogs
-from uwstyle.dialogs.slctbox import Combobox, Choose
+import uwstyle
+import warnings
 
-DIALOGBUTTON_OK = Dialogs.BUTTON_OK
-DIALOGBUTTON_OKCANCEL = Dialogs.BUTTON_OK | Dialogs.BUTTON_CANCEL
-DIALOGBUTTON_YESNO = Dialogs.BUTTON_YES | Dialogs.BUTTON_NO
-DIALOGBUTTON_RETRYIGNOREABORT = Dialogs.BUTTON_ABORT | Dialogs.BUTTON_IGNORE | Dialogs.BUTTON_RETRY
+DIALOGBUTTON_OK = 1
+DIALOGBUTTON_OKCANCEL = 3
+DIALOGBUTTON_YESNO = 12
+DIALOGBUTTON_RETRYIGNOREABORT = 112
 
 def dialog(message, timeout=0, buttons=DIALOGBUTTON_OK):
   """
@@ -37,9 +37,8 @@ def dialog(message, timeout=0, buttons=DIALOGBUTTON_OK):
     * "abort" if ABORT.
     None if the dialog times out when the X button is pressed.
   """
-  box = Dialogs(__name__, message, buttons)
-  box.timeout = timeout
-  return _retcode2bool(box.show())
+  warnings.warn("This Method moved uwstyle.dialog()", DeprecationWarning)
+  return uwstyle.dialog(message, timeout, buttons)
 
 def select(message, items=[], timeout=0, buttons=DIALOGBUTTON_OK):
   """
@@ -74,10 +73,8 @@ def select(message, items=[], timeout=0, buttons=DIALOGBUTTON_OK):
     3: int
       Selected Item index
   """
-  box = Combobox(__name__, message, buttons)
-  box.timeout = timeout
-  box.items = items
-  return _retcode2bool(box.show())
+  warnings.warn("This Method moved uwstyle.select()", DeprecationWarning)
+  return uwstyle.select(message, items, timeout, buttons)
 
 
 def choose(message, items=[], timeout=0):
@@ -103,48 +100,8 @@ def choose(message, items=[], timeout=0):
     2: str
       Selected Items
   """
-  box = Choose(__name__, message)
-  box.timeout = timeout
-  box.items = items
-  return box.show()
-
-def _retcode2bool(retcode):
-  """
-  Converts the return code of a dialog to the return value of a method.
-  * True if OK, YES.
-  * False if CANCEL, NO.
-  * "retry" if RETRY.
-  * "ignore" if IGNORE.
-  * "abort" if ABORT.
-  * None if None.
-  is returned.
-
-  Parameters
-  ----
-  retcode: int|tuple
-    retcode
-  """
-  ret = None
-  if type(retcode) is tuple:
-    ret = retcode[0]
-  else:
-    ret = retcode
-
-  if ret == Dialogs.BUTTON_YES or ret == Dialogs.BUTTON_OK:
-    ret = True
-  elif ret == Dialogs.BUTTON_NO or ret == Dialogs.BUTTON_CANCEL:
-    ret = False
-  elif ret == Dialogs.BUTTON_ABORT:
-    ret = "abort"
-  elif ret == Dialogs.BUTTON_RETRY:
-    ret = "retry"
-  elif ret == Dialogs.BUTTON_IGNORE:
-    ret = "ignore"
-
-  if type(retcode) == tuple:
-    ret = tuple([ret]) + retcode[1:]
-
-  return ret
+  warnings.warn("This Method moved uwstyle.choose()", DeprecationWarning)
+  return uwstyle.choose(message, items, timeout)
 
 if __name__ == "__main__":
   print(dialog("test"))
@@ -166,3 +123,4 @@ if __name__ == "__main__":
   print(dialog("""
   this is a long long text dialog
   """ * 100))
+
